4f05c1c9a1a749660540cb04863b560958e4bdef
voipoddsns <voipoddsns@gmail.com>
2026-05-08 15:56:14 +0300

    Refactor unic_id initialization and configuration setup

--- ts.js
+++ ts.js
@@ -1,15 +1,27 @@
 (function () {
     'use strict';
-	
-    var unic_id = Lampa.Storage.get('lampac_unic_id', '');
-    if (!unic_id) {
-      unic_id = Lampa.Utils.uid(8).toLowerCase();
-      Lampa.Storage.set('lampac_unic_id', unic_id);
+
+    // 1. Инициализация unic_id
+    let unicId = Lampa.Storage.get('lampac_unic_id', '');
+
+    if (!unicId) {
+        // Если ID не найден, генерируем новый и сохраняем
+        unicId = Lampa.Utils.uid(8).toLowerCase();
+        Lampa.Storage.set('lampac_unic_id', unicId);
     }
 
-    Lampa.Storage.set('torrserver_url','http://192.168.1.17:8090');
-    Lampa.Storage.set('torrserver_auth','false');
-    Lampa.Storage.set('torrserver_login',Lampa.Storage.get('account_email') || Lampa.Storage.get('lampac_unic_id', '') || 'ts');
-    Lampa.Storage.set('torrserver_password','ts');
-	
+    // 2. Установка конфигурационных параметров
+    Lampa.Storage.set('torrserver_url', 'http://192.168.1.17:8090');
+    Lampa.Storage.set('torrserver_auth', 'false');
+
+    // 3. Установка логина (используем явную цепочку для ясности)
+    const accountEmail = Lampa.Storage.get('account_email');
+    const existingUnicId = Lampa.Storage.get('lampac_unic_id', '');
+    
+    const login = accountEmail || existingUnicId || 'ts';
+    Lampa.Storage.set('torrserver_login', login);
+    
+    // 4. Установка пароля
+    Lampa.Storage.set('torrserver_password', 'ts');
+
 })();
