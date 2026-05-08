7e6b9fbbdd2e84ec1e3bb5a50ef5cfe750f3ccc3
voipoddsns <voipoddsns@gmail.com>
2026-05-08 15:45:05 +0300

    Add initial setup for Lampa storage configuration

---
+++ ts.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+	
+    var unic_id = Lampa.Storage.get('lampac_unic_id', '');
+    if (!unic_id) {
+      unic_id = Lampa.Utils.uid(8).toLowerCase();
+      Lampa.Storage.set('lampac_unic_id', unic_id);
+    }
+
+    Lampa.Storage.set('torrserver_url','http://192.168.1.17:8090');
+    Lampa.Storage.set('torrserver_auth','true');
+    Lampa.Storage.set('torrserver_login',Lampa.Storage.get('account_email') || Lampa.Storage.get('lampac_unic_id', '') || 'ts');
+    Lampa.Storage.set('torrserver_password','ts');
+	
+})();
